ffd183ca6c9888eb1f8a99c220265f6c7823c6f1
Nini <ninichang7066@gmail.com>
2020-12-30 22:01:58 +0800

    chore: init home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Home />
     </div>
   );
 }
