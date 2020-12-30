282ee97bf1dc43fd04a5e94f1a3cbfd498e49ca2
Nini <ninichang7066@gmail.com>
2020-12-30 21:45:02 +0800

    chore: add firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCWy-oz-7bounthGresXt4J6C2QiErbVWU",
+  authDomain: "clone-react-c8950.firebaseapp.com",
+  projectId: "clone-react-c8950",
+  storageBucket: "clone-react-c8950.appspot.com",
+  messagingSenderId: "347875820361",
+  appId: "1:347875820361:web:b62372a650373034a99098",
+  measurementId: "G-8S6KPJCJYP"
+};
