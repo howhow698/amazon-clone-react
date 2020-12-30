02b29cabb00d381a43ee6956f768d0bb8d1ebf1b
James <james101511@yahoo.com.tw>
2020-12-30 22:57:44 +0800

    feat: add css for the options in nav (53:44)

--- src/Header.js
+++ src/Header.js
@@ -1,27 +1,40 @@
-import './Header.css';
+import "./Header.css";
 
-import React from 'react';
+import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
-    return (
-        <div className = 'header'>
-            <img className='header_logo' src='https://pngimg.com/uploads/amazon/amazon_PNG11.png'></img>
-            <div className='header_search'>
-             <input type = 'text'></input>
-            </div>
-            <div className='header_nav'>
-                <div className='header_option'>
-                    <span className='header_optionLineOne'>hello motor</span>
-                    <span className='header_optionLinetwo'>hello motor</span>
-                </div>
-                <div className='header_option'>
-                    <span className='header_optionLineOne'>hello motor</span>
-                    <span className='header_optionLinetwo'>hello motor</span>
-                </div>
-                <div className='header_option'></div>
-            </div>
+  return (
+    <div className="header">
+      <img
+        className="header_logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+      ></img>
+      <div className="header_search">
+        <input type="text" className="searchInput"></input>
+        <SearchIcon className="searchIcon"></SearchIcon>
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionLineOne">hello motor</span>
+          <span className="header_optionLinetwo">Sign in</span>
         </div>
-    )
+        <div className="header_option">
+          <span className="header_optionLineOne">hello motor</span>
+          <span className="header_optionLinetwo">hello motor</span>
+        </div>
+        <div className="header_option">
+          <span className="header_optionLineOne">hello motor</span>
+          <span className="header_optionLinetwo">hello motor</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingBasket></ShoppingBasket>
+          <span className="header_optionLinetwo header_basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
