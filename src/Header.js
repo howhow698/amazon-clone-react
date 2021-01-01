5e2ae75a48ba0e445eed2efb2858027c0b7dbd66
Nini <ninichang7066@gmail.com>
2021-01-01 14:40:27 +0800

    chore: add react-router-dom

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
+
+// material-ui
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 function Header() {
   return (
     <div className="header">
-      <a href="https://www.amazon.com/amazonprime/137-0254394-3237200?_encoding=UTF8&%2AVersion%2A=1&%2Aentries%2A=0">
+      <Link to="/">
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
-      </a>
+      </Link>
       <div className="header__search">
         <input className="header__searchInput" type="text" />
         <SearchIcon className="header__searchIcon" />
@@ -29,10 +32,12 @@ function Header() {
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
         </div>
-        <div className="header__optionShopCart">
-          <ShoppingCartIcon />
-          <div class="header__optionLineTwo header__optionShopCartCount">0</div>
-        </div>
+        <Link to="/checkout" className="header__optionShopCartLink">
+          <div className="header__optionShopCart">
+            <ShoppingCartIcon />
+            <div class="header__optionLineTwo header__optionShopCartCount">0</div>
+          </div>
+        </Link>
       </div >
     </div>
   )
