b1a4cf9bec6299f8da6a9c07f80757e35f13b921
Nini <ninichang7066@gmail.com>
2021-01-06 17:45:03 +0800

    feature: (firebase) add firebase and modify config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,4 +1,11 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+/**
+ * For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ * 
+ * Firebase: amazon-clone-react
+ * domain: clone-react-c8950
+ */
 const firebaseConfig = {
   apiKey: "AIzaSyCWy-oz-7bounthGresXt4J6C2QiErbVWU",
   authDomain: "clone-react-c8950.firebaseapp.com",
@@ -8,3 +15,9 @@ const firebaseConfig = {
   appId: "1:347875820361:web:b62372a650373034a99098",
   measurementId: "G-8S6KPJCJYP"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
